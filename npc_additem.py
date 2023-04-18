d5aeb645d0ebbdbdf7883e1d356c83eed91ec5d6
Bearded InfoSec <abarbas19@gmail.com>
2023-04-18 15:31:09 -0500

    added option to mass delete

--- npc_additem.py
+++ npc_additem.py
@@ -8,14 +8,11 @@ f = 'item_list.txt'
 if not os.path.exists(f):
     f = open('item_list.txt','w')
 cleaned = open('cleaned_list.txt','a')
+
 g = int(input('Add Wowhead urls to "item_list.txt"\n1. Done\n2. Not yet\n'))
 while g != 1:
     g = int(input('\nAre you finished adding items to "item_list.txt"?\n1. Yes\n2. No'))
-gmcommand = input("\nenter the gm command (.additem)\n")
-review = input('The command you entered was:\n {}\n was this correct?:\n1. Yes\n2. No(retype command)\n'.format(gmcommand))
-while int(review) != 1:
-    review = input('Please re-enter your gm command:\n')
-gmcommand = review
+
 for i in f:
     x = i.strip('https://www.wowhead.com/classic/item=')
     x = i.strip('https://www.wowhead.com/tbc/item=')
@@ -23,24 +20,44 @@ for i in f:
     x = x.strip('\n')
     v = x.split('/')
     itemList.append(v[0])
-for c in itemList:
-    if c not in de_duped:
-        cleaned.write("{} {} 0 0 0 \n".format(gmcommand,c))
-        de_duped.append(c) 
+
+s = False
+while not s:
+    c = input('\n1. Add Item(s)\n2. Delete Item(s)\n')
+    if int(c) == 1:
+        for c in itemList:
+            if c not in de_duped:
+                cleaned.write(".npc additem {} 0 0 0 \n".format(c))
+                de_duped.append(c)
+        break
+    
+    if int(c) == 2:
+        for c in itemList:
+            if c not in de_duped:
+                cleaned.write(".npc delitem {} 0 0 0 \n".format(c))
+                de_duped.append(c)
+        break
+    
 cleaned.close()    
-start = input('Are you targeting npc?\n1. Yes\n2. No\n')
-while int(start) != 1:
-    print("\nplease target the NPC then press 1 to continue: ")
-print('\nCommands will run to add items in 10 seconds...')
-time.sleep(10)
+
+print('\nSwitch to your World of Warcraft client and target the NPC\nCommands will run in 10 seconds...')
+
+for i in range(10, 0, -1):
+    print(f'{i} seconds left...')
+    time.sleep(1)
+
+print('Starting commands...')
+
 cleaned = open('cleaned_list.txt', 'r')
+
 lines = cleaned.readlines()
 for line in lines:
     py.press('enter')
     print(line)
     for char in line.strip():
         py.press(char)
-    py.press('enter')  
+    py.press('enter')
+
 cleaned.close()
 os.remove("cleaned_list.txt")
 
